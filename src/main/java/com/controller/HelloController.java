f13f4defe63d99ffb5d3b1c0222d275a5261016b
=samirvithlani <=samir.vithlani83955@gmail.com>
2020-06-08 11:31:44 +0530

    first deploy

---
+++ src/main/java/com/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloController
+ */
+public class HelloController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("hello controller....");
+		response.sendRedirect("hello.jsp");
+		
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1><a href="HelloController">HELLO</a></h1>
+</body>
+</html>
